29c4648d6e99679b14a3bbfdf28700932565e4c0
Jaison <jaisonschmidt@gmail.com>
2024-09-19 21:24:54 -0300

    Funcionalidades de senha

--- script.js
+++ script.js
@@ -6,6 +6,13 @@ document.addEventListener('DOMContentLoaded', function() {
 const formLogin = document.getElementById("formLogin");
 const txtEmail = document.getElementById("txtEmail"); // referência para o campo de email
 const txtSenha = document.getElementById("txtSenha"); // referência para o campo de senha
+const alerta = document.getElementById("alerta");
+const msgErro = document.getElementById("msgErro"); 
+
+function showErrorMessage(message) {
+    msgErro.innerText = message;
+    alerta.classList.add("show");
+}
 
 formLogin.addEventListener("submit", function(event) {
     event.preventDefault();
@@ -21,28 +28,32 @@ formLogin.addEventListener("submit", function(event) {
 
     // verificar se o email é válido
     if (email === "") {
-        alert("O campo de email é obrigatório!");
+        showErrorMessage("O campo de email é obrigatório!");
         return;
     }
 
     // verificar se o email tem arroba
     if (!email.includes("@")) {
-        alert("O email deve conter o caractere @!");
+        showErrorMessage("O email deve conter o caractere @!");
         return;
     }
     
     // verificar se a senha é válida
     if (senha === "") {
-        alert("O campo de senha é obrigatório!");
+        showErrorMessage("O campo de senha é obrigatório!");
         return;
     }
 
     // Verificar se a senha tem no mínimo 3 caracteres
     if (senha.length < 3) {
-        alert("A senha deve ter no mínimo 3 caracteres!");
+        showErrorMessage("A senha deve ter no mínimo 3 caracteres!");
         return;
     }
 
     // caso não seja, apresentar uma mensagem de erro ao usuário
     // caso seja, apresentar uma mensagem de login efetuado com sucesso
+
+    alert('Login efetuado com sucesso!');
+    // esconder a mensagem de erro
+    
 });
